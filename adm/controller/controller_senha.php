b84bd8a88e2f72e70d04eab6cc6df151392c8610
stela montenegro <stelamontenegro21@gmail.com>
2023-08-28 21:05:55 -0300

    ESQUECE
    essa merda desse email e desse sms, fds. foca nas procedures pra terminar logo

--- adm/controller/controller_senha.php
+++ adm/controller/controller_senha.php
@@ -12,50 +12,10 @@ if(isset($_REQUEST["verif_tel"])){ //se vier do admNew (criar adm)
     if($resp["result"]==1){ //tudo certo
       
 
-        require '../../PHPMailer-master/PHPMailer-master/src/Exception.php';
-        require '../..//PHPMailer-master/PHPMailer-master/src/PHPMailer.php';
-        require '../../PHPMailer-master/PHPmailer-master/src/SMTP.php';
        
-
-$para = "stelamontenegro@gmail.com";
-$assunto = "Teste php";
-
-// Corpo do e-mail usando HTML
-$corpo = "<p> Olá Stela,</br>Seu código de verificação é: 124525</br>Use este código para concluir o processo de verificação.Ele é válido por 5 minutos.<br>Obrigado,<br>Equipe de Suporte Yara</p>";
-
-// Crie uma instância do PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();;
-
-try {
-    // Configurações do servidor SMTP do Gmail
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'stelamontenegro3@gmail.com';
-    $mail->Password   = '#stela0312';
-    $mail->SMTPSecure = 'tls'; // Use 'ssl' se preferir
-    $mail->Port       = 587;   // Use 465 se estiver usando SSL
-
-    // Destinatário e remetente
-    $mail->setFrom('stelamontenegro3@gmail.com', 'Seu Nome');
-    $mail->addAddress('stelasmontenegro@gmail.com');
-
-    // Conteúdo do e-mail
-    $mail->isHTML(true);
-    $mail->Subject = "Teste php";
-    $mail->Body    = "<p> Olá Stela,</br>Seu código de verificação é: 124525</br>Use este código para concluir o processo de verificação.Ele é válido por 5 minutos.<br>Obrigado,<br>Equipe de Suporte Yara</p>";
-
-    // Envie o e-mail
-    $mail->send();
-    echo "E-mail enviado com sucesso";
-} catch (Exception $e) {
-    echo "Erro ao enviar o e-mail: {$mail->ErrorInfo}";
-}
-
-        ?>
         
-    
-        <?php  
+
+        
     }else{//erro
         ?>
         <form action="../view/senhas.php" name="form" id="myForm" method="POST">
