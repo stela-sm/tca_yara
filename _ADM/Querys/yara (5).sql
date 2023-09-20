5f2614f39905a12ed93256b16854dbeb08d383b1
stela montenegro <stelamontenegro21@gmail.com>
2023-09-20 20:40:59 -0300

    DATABASE

--- _ADM/Querys/yara (5).sql
+++ _ADM/Querys/yara (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24-Ago-2023 às 02:04
+-- Tempo de geração: 21-Set-2023 às 01:39
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -21,6 +21,48 @@ SET time_zone = "+00:00";
 -- Banco de dados: `yara`
 --
 
+DELIMITER $$
+--
+-- Procedimentos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CheckEmailCPF` (IN `p_email` VARCHAR(255), IN `p_cpf` VARCHAR(11), OUT `p_result` INT, OUT `p_user_id` INT)   BEGIN
+    DECLARE email_count INT;
+    DECLARE cpf_count INT;
+    
+    SELECT COUNT(*) INTO email_count FROM adm WHERE email = p_email;
+    SELECT COUNT(*) INTO cpf_count FROM adm WHERE cpf = p_cpf;
+
+    IF email_count > 0 OR cpf_count > 0 THEN
+        SET p_result = 1;
+        
+        -- Se um usuário for encontrado, pegue o ID
+        SELECT ID_ADM INTO p_user_id FROM adm WHERE email = p_email OR cpf = p_cpf;
+    ELSE
+        SET p_result = 0;
+        SET p_user_id = NULL; -- Defina como NULL se nenhum usuário for encontrado
+    END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CheckEmailCPFCliente` (IN `p_email` VARCHAR(255), IN `p_cpf` VARCHAR(11), OUT `p_result` INT, OUT `p_user_id` INT)   BEGIN
+    DECLARE email_count INT;
+    DECLARE cpf_count INT;
+    
+    SELECT COUNT(*) INTO email_count FROM cliente WHERE email = p_email;
+    SELECT COUNT(*) INTO cpf_count FROM cliente WHERE cpf = p_cpf;
+
+    IF email_count > 0 OR cpf_count > 0 THEN
+        SET p_result = 1;
+        
+        -- Se um usuário for encontrado, pegue o ID
+        SELECT ID_CLIENTE INTO p_user_id FROM cliente WHERE email = p_email OR cpf = p_cpf;
+    ELSE
+        SET p_result = 0;
+        SET p_user_id = NULL; -- Defina como NULL se nenhum usuário for encontrado
+    END IF;
+END$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -31,6 +73,7 @@ CREATE TABLE `adm` (
   `ID_ADM` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
   `senha` varchar(64) NOT NULL,
   `celular` varchar(20) NOT NULL,
   `poder` int(11) DEFAULT NULL,
@@ -42,11 +85,9 @@ CREATE TABLE `adm` (
 -- Extraindo dados da tabela `adm`
 --
 
-INSERT INTO `adm` (`ID_ADM`, `nome`, `email`, `senha`, `celular`, `poder`, `status`, `datahora`) VALUES
-(1, 'Stela dos Santos', 'stela3@gmail.com', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', '11963220905', 5, '1', '2023-08-21 18:08:49'),
-(2, 'clara', 'clarasaches@gmail.com', '12345', '11993832299', 7, '1', '2023-08-11 18:02:12'),
-(3, 'samuel', 'stela@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '11963220905', 2, '1', '2023-08-12 17:47:29'),
-(5, 'ana', 'stelamontenegro3@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '11963220905', 1, '1', '2023-08-12 17:49:15');
+INSERT INTO `adm` (`ID_ADM`, `nome`, `email`, `cpf`, `senha`, `celular`, `poder`, `status`, `datahora`) VALUES
+(1, 'Stela ', 'stela@gmail.com', '40527647810', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '11963220905', 5, '1', '2023-09-14 17:25:48'),
+(6, 'samuel', 'samuel@gmail.com', '16511604810', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '11993832299', 1, '1', '2023-08-29 20:34:10');
 
 -- --------------------------------------------------------
 
@@ -59,8 +100,15 @@ CREATE TABLE `carrinho` (
   `id_cliente` int(11) DEFAULT NULL,
   `id_produto` int(11) DEFAULT NULL,
   `quantidade` int(11) DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL
+  `preco` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `carrinho`
+--
+
+INSERT INTO `carrinho` (`ID_CARRINHO`, `id_cliente`, `id_produto`, `quantidade`, `preco`) VALUES
+(1, 1, 2, 2, 180);
 
 -- --------------------------------------------------------
 
@@ -74,16 +122,17 @@ CREATE TABLE `cliente` (
   `cpf` varchar(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `datan` date DEFAULT NULL,
-  `datahora` timestamp NOT NULL DEFAULT current_timestamp()
+  `celular` varchar(15) NOT NULL,
+  `datahora` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL COMMENT '1 - ativo: 0-inativo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `cliente`
 --
 
-INSERT INTO `cliente` (`ID_CLIENTE`, `nome`, `cpf`, `email`, `senha`, `datan`, `datahora`) VALUES
-(1, 'stela', '40527647810', 's@s.com', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', '0000-00-00', '2023-08-23 00:05:42');
+INSERT INTO `cliente` (`ID_CLIENTE`, `nome`, `cpf`, `email`, `senha`, `celular`, `datahora`, `status`) VALUES
+(1, 'stela', '40527647810', 's@s.com', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', '11963220905', '2023-08-25 23:03:25', 1);
 
 -- --------------------------------------------------------
 
@@ -161,31 +210,6 @@ INSERT INTO `itens` (`ID_ITENS`, `id_pedido`, `id_produto`, `quantidade`, `prese
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `menu`
---
-
-CREATE TABLE `menu` (
-  `ID_MENU` int(11) NOT NULL,
-  `folder` varchar(1) NOT NULL COMMENT 'pasta onde se encontra a pagina [i] ou [v]',
-  `nome` varchar(300) NOT NULL COMMENT 'nome do menu/link',
-  `url` varchar(300) NOT NULL COMMENT 'url destino do link',
-  `datahora` datetime NOT NULL COMMENT 'data e hora do registro yyy-mm-dd hh:mm:ss',
-  `status` int(1) NOT NULL COMMENT '1-ativo; 0-inativo'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `menu`
---
-
-INSERT INTO `menu` (`ID_MENU`, `folder`, `nome`, `url`, `datahora`, `status`) VALUES
-(25, 'v', 'Shopping', 'shop.php', '2023-08-17 19:29:37', 0),
-(26, 'r', 'Shop', 'view/shop.php', '2023-08-17 19:29:37', 1),
-(27, 'v', 'Sobre', 'Sobre.php', '2023-08-21 15:33:18', 1),
-(28, 'r', 'Sobre', 'view/Sobre.php', '2023-08-21 15:33:18', 1);
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `pedidos`
 --
 
@@ -204,7 +228,32 @@ CREATE TABLE `pedidos` (
 --
 
 INSERT INTO `pedidos` (`ID_PEDIDO`, `id_cliente`, `id_endereco`, `valor`, `pagamento`, `status`, `datahora`) VALUES
-(1, 1, 1, '50.00', 'crédito', 'finalizade', '2023-02-22 21:09:36');
+(1, 1, 1, '50.00', 'crédito', '3', '2023-08-24 20:26:10');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos_status`
+--
+
+CREATE TABLE `pedidos_status` (
+  `ID_STATUS` int(11) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `pedidos_status`
+--
+
+INSERT INTO `pedidos_status` (`ID_STATUS`, `status`) VALUES
+(1, 'Aguardando Pagamento'),
+(2, 'Em preparação'),
+(3, 'Em rota de entrega'),
+(4, 'Finalizado'),
+(5, 'Aguardando devolução'),
+(6, 'Estornado'),
+(7, 'Cancelado'),
+(8, 'Tentativa de entrega');
 
 -- --------------------------------------------------------
 
@@ -217,6 +266,7 @@ CREATE TABLE `produtos` (
   `nome` varchar(255) DEFAULT NULL,
   `descricao` text DEFAULT NULL,
   `img` varchar(100) NOT NULL,
+  `img_sec` varchar(100) NOT NULL,
   `img_hover` varchar(100) NOT NULL,
   `categoria` int(11) DEFAULT NULL,
   `finalidade` int(11) DEFAULT NULL,
@@ -231,33 +281,8 @@ CREATE TABLE `produtos` (
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`ID_PRODUTO`, `nome`, `descricao`, `img`, `img_hover`, `categoria`, `finalidade`, `estoque`, `valor_uni`, `ingredientes`, `status`, `datahora`) VALUES
-(2, 'Creme aloe vera', 'Formulado com aloe vera de alta qualidade, este creme exclusivo oferece uma experiência de cuidado da pele como nenhuma outra. A aloe vera, conhecida por suas propriedades hidratantes e curativas, é o segredo para uma pele saudável e radiante.', 'produto_UAaRUhol_img_.jpeg', 'produto_WufLjgBI_imghover_.png', 1, 1, 2225, '89.99', 'Gel de Aloe Vera: 60% (proporção maior para maximizar os benefícios)\r\nÓleo de Jojoba: 10%\r\nÓleo de Coco Fracionado: 8%\r\nGlicerina Vegetal: 5%\r\nManteiga de Karité: 5%\r\nEmulsificante Natural (como cera de abelha ou emulsificante vegetal): 6%\r\nÁgua Destilada: 4%\r\nConservante Natural (como extrato de to', '1 ', '2023-08-15 17:14:49');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `submenu`
---
-
-CREATE TABLE `submenu` (
-  `ID_SUBMENU` int(11) NOT NULL,
-  `folder` varchar(1) NOT NULL COMMENT 'index [i] oi view [v]',
-  `ID_MENU_FK` int(3) NOT NULL COMMENT 'id da tabela menu',
-  `nomesub` varchar(100) NOT NULL COMMENT 'nome do link',
-  `url` varchar(300) NOT NULL COMMENT 'url do link',
-  `datahora` datetime NOT NULL COMMENT 'data e hora',
-  `status` int(1) NOT NULL COMMENT '1-ativo;0-inativo'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `submenu`
---
-
-INSERT INTO `submenu` (`ID_SUBMENU`, `folder`, `ID_MENU_FK`, `nomesub`, `url`, `datahora`, `status`) VALUES
-(14, 'v', 26, 'kitss', 'view/kits.php', '2023-08-17 20:28:05', 0),
-(15, 'v', 25, 'produtos', 'produtos.php', '2023-08-17 20:40:02', 0),
-(16, 'r', 26, 'produtos', 'view/produtos.php', '2023-08-17 20:40:02', 1);
+INSERT INTO `produtos` (`ID_PRODUTO`, `nome`, `descricao`, `img`, `img_sec`, `img_hover`, `categoria`, `finalidade`, `estoque`, `valor_uni`, `ingredientes`, `status`, `datahora`) VALUES
+(2, 'Creme aloe vera', 'Formulado com aloe vera de alta qualidade, este creme exclusivo oferece uma experiência de cuidado da pele como nenhuma outra. A aloe vera, conhecida por suas propriedades hidratantes e curativas, é o segredo para uma pele saudável e radiante.', 'produto_UAaRUhol_img_.jpeg', 'produto_ackdASgG_imgsec_.png', 'produto_WufLjgBI_imghover_.png', 1, 1, 2225, '89.99', 'Gel de Aloe Vera: 60% (proporção maior para maximizar os benefícios)\r\nÓleo de Jojoba: 10%\r\nÓleo de Coco Fracionado: 8%\r\nGlicerina Vegetal: 5%\r\nManteiga de Karité: 5%\r\nEmulsificante Natural (como cera de abelha ou emulsificante vegetal): 6%\r\nÁgua Destilada: 4%\r\nConservante Natural (como extrato de to', '1 ', '2023-09-19 20:46:59');
 
 --
 -- Índices para tabelas despejadas
@@ -268,7 +293,8 @@ INSERT INTO `submenu` (`ID_SUBMENU`, `folder`, `ID_MENU_FK`, `nomesub`, `url`, `
 --
 ALTER TABLE `adm`
   ADD PRIMARY KEY (`ID_ADM`),
-  ADD UNIQUE KEY `unique_email` (`email`);
+  ADD UNIQUE KEY `unique_email` (`email`),
+  ADD UNIQUE KEY `cpfadm` (`cpf`);
 
 --
 -- Índices para tabela `carrinho`
@@ -282,7 +308,9 @@ ALTER TABLE `carrinho`
 -- Índices para tabela `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`ID_CLIENTE`);
+  ADD PRIMARY KEY (`ID_CLIENTE`),
+  ADD UNIQUE KEY `cpf` (`cpf`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Índices para tabela `endereco`
@@ -306,18 +334,18 @@ ALTER TABLE `itens`
   ADD KEY `id_produto` (`id_produto`);
 
 --
--- Índices para tabela `menu`
---
-ALTER TABLE `menu`
-  ADD PRIMARY KEY (`ID_MENU`);
-
---
 -- Índices para tabela `pedidos`
 --
 ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`ID_PEDIDO`),
   ADD KEY `id_cliente` (`id_cliente`),
   ADD KEY `id_endereco` (`id_endereco`);
+
+--
+-- Índices para tabela `pedidos_status`
+--
+ALTER TABLE `pedidos_status`
+  ADD PRIMARY KEY (`ID_STATUS`);
 
 --
 -- Índices para tabela `produtos`
@@ -328,13 +356,6 @@ ALTER TABLE `produtos`
   ADD KEY `finalidade` (`finalidade`);
 
 --
--- Índices para tabela `submenu`
---
-ALTER TABLE `submenu`
-  ADD PRIMARY KEY (`ID_SUBMENU`),
-  ADD KEY `ID_MENU_FK` (`ID_MENU_FK`);
-
---
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -342,19 +363,19 @@ ALTER TABLE `submenu`
 -- AUTO_INCREMENT de tabela `adm`
 --
 ALTER TABLE `adm`
-  MODIFY `ID_ADM` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID_ADM` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `carrinho`
 --
 ALTER TABLE `carrinho`
-  MODIFY `ID_CARRINHO` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_CARRINHO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `ID_CLIENTE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID_CLIENTE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `endereco`
@@ -375,28 +396,22 @@ ALTER TABLE `itens`
   MODIFY `ID_ITENS` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de tabela `menu`
---
-ALTER TABLE `menu`
-  MODIFY `ID_MENU` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
-
---
 -- AUTO_INCREMENT de tabela `pedidos`
 --
 ALTER TABLE `pedidos`
   MODIFY `ID_PEDIDO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT de tabela `pedidos_status`
+--
+ALTER TABLE `pedidos_status`
+  MODIFY `ID_STATUS` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
   MODIFY `ID_PRODUTO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `submenu`
---
-ALTER TABLE `submenu`
-  MODIFY `ID_SUBMENU` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restrições para despejos de tabelas
